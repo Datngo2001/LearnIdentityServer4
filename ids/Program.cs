956ea54f3d475ae0028ec15df6d9bc5196753437
DatNgo <ngominhdat110115@gmail.com>
2023-01-16 16:56:09 +0700

    add api

--- ids/Program.cs
+++ ids/Program.cs
@@ -1,6 +1,21 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using ids;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddTestUsers(Config.Users)
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseIdentityServer();
 
 app.Run();
